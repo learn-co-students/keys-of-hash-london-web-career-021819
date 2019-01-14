fe59d2186b58658ce1ad8e60d4319d9b120e0e05
Calie Rushton <calielrushton@gmail.com>
2019-01-14 10:22:23 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+#require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments) # undefined no of args
+    result = []
+      self.each do |key, value| # self points to hash
+        arguments.each do |arg| # splat converted arguments into an array of arguments
+          if value == arg
+            result << key
+          end
+        end
+      end
+    result
   end
 end
