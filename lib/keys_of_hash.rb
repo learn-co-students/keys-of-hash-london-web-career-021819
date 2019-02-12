86c3071c67af52046b778fbb178467d9317ac859
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2019-02-12 14:22:35 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,14 +1,20 @@
+# class Hash
+#   def keys_of(*arguments)
+#     array = []
+#     arguments.each do |key, value|
+#       if arguments.include?(value) 
+#         array << key
+#       end
+#     end
+#     return array
+#   end
+# end
+
 class Hash
-  def keys_of(*arguments)
-    array = []
-    arguments.each do |key, value|
-      if arguments.include?(value) 
-        array << key
-      end
-    end
-    return array
+  def keys_of(*args)
+    map {|key, value| args.include?(value) ? key : nil }.compact
   end
 end
 
 
-#self 
+#My code commented out above does the same thing as this code (I think) but mine doesnt work (lol)
